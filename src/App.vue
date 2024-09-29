8a73c8892f160023c3e7e357d723d79bf61011f3
AmrMustafa13 <amrmostafa1311@gmail.com>
2024-09-29 19:29:25 +0300

    add income expenses component

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
   <Header />
   <div class="container">
     <Balance />
+    <IncomeExpenses />
   </div>
 </template>
 
 <script setup>
 import Header from "./components/Header.vue";
 import Balance from "./components/Balance.vue";
+import IncomeExpenses from "./components/IncomeExpenses.vue";
 </script>
